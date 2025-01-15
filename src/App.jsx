0cc790e37d3e5796fb57741ec36cbf1ffe1ddf1f
Matthew Price <mattprice456@live.com>
2025-01-15 10:26:25 -0500

    Changes to exercise 2.1 as per requirement of changing parts array size i.e. dynamically created elements on a varying input array

--- src/App.jsx
+++ src/App.jsx
@@ -2,57 +2,49 @@ const Header = ({ course }) => <h1>{course}</h1>
 
 const Total = ({ sum }) => <p>Number of exercises {sum}</p>
 
-const Part = ({ part }) => 
+const Part = ({ part }) =>
   <p>
     {part.name} {part.exercises}
   </p>
 
-const Content = ({ parts }) => 
+const Content = ({ parts }) =>
   <>
-    <Part
-      part={parts[0]} 
-    />
-    <Part
-      part={parts[1]} 
-    />
-    <Part
-      part={parts[2]} 
-    />      
+    {parts.map(parts => <Part key={parts.id} part={parts} />)}
   </>
 
-  const Course = ({ course }) => {
-    return (
-      <div>
-        <Header course={course.name} />
-        <Content parts={course.parts} />
-      </div>
-    )
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
   }
 
-  const App = () => {
-    const course = {
-      id: 1,
-      name: 'Half Stack application development',
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        }
-      ]
-    }
-  
-    return <Course course={course} />
-  }
+  return <Course course={course} />
+}
 
 export default App
