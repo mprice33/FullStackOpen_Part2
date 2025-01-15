0c0b5057c6fca9d3adb8e706ed17452c00374291
Matthew Price <mattprice456@live.com>
2025-01-15 11:39:19 -0500

    Refactoring repo to have multiple projects, adding answer for exercise 2.6

---
+++ PhoneBook/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    let newPerson = { name: newName }
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+  
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <br></br>
+      <div>{persons.map(person=><p key={person.name}>{person.name}</p>)}</div>
+    </div>
+  )
+}
+
+export default App
