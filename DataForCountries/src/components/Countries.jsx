3bcd2c226e429265da8620a45e56f828e49599d7
Matthew Price <mattprice456@live.com>
2025-01-27 11:22:26 -0500

    Refactored Countries component to handle a togglable show button for the country component as per 2.19

--- DataForCountries/src/components/Countries.jsx
+++ DataForCountries/src/components/Countries.jsx
@@ -1,13 +1,36 @@
 import Country from "../components/Country"
+import { useState } from "react"
 const Countries = ({countries}) => {
-  console.log(countries)
+
+  const [shownList, setShownList] = useState([])
+
+  const handleShow = (country) => {
+    shownList.includes(country) ? setShownList(shownList.filter(c=>c!==country)) : setShownList(shownList.concat(country))
+  }
+  
   if (countries.length > 10){
     return <p>Too many matches, specify another filter</p>
   }else if (countries.length === 1){
     return <Country country={countries[0]}/>
   }
   else{
-    return countries.map(country=><p key={country.name.common}>{country.name.common}</p>)
+    return countries.map(country=>{
+      if(shownList.includes(country)){
+        return (
+          <div key={country.name.common}>
+            <p>{country.name.common} <button onClick={()=>handleShow(country)}>hide</button></p>
+            <Country country={country}/>
+          </div>
+        )
+      }else{
+      return (
+        <div key={country.name.common}>
+        <p>{country.name.common} <button onClick={()=>handleShow(country)}>show</button></p>
+      </div>
+      )
+    }
+  }
+  )
   }
 }
 
