daf8eb05f3b75afba2e63039aaaedf97b1a22af1
Matthew Price <mattprice456@live.com>
2025-01-27 12:17:47 -0500

    Adding weather details of the capital to the country component for toggle/single select as per 2.20, API key to OpenWeatherMap must be provided as part of the environment variables to run

--- DataForCountries/src/components/Country.jsx
+++ DataForCountries/src/components/Country.jsx
@@ -1,4 +1,17 @@
+import countryService from "../services/Country"
+import { useState, useEffect } from "react"
+
 const Country = ({ country }) => {
+
+    const [weather, setWeather] = useState({})
+    useEffect(() => {
+        countryService.getWeatherByLatLong(country.capitalInfo.latlng[0], country.capitalInfo.latlng[1])
+            .then(weather => {
+                console.log(weather)
+                setWeather(weather)
+            })
+    }, [country.latlng])
+
     return <div>
         <h1>{country.name.common}</h1>
         <p>capital: {country.capital[0]}</p>
@@ -8,6 +21,10 @@ const Country = ({ country }) => {
             {Object.values(country.languages).map(language => <li key={language}>{language}</li>)}
         </ul>
         <img src={country.flags.svg} alt={country.flags.alt} width="200px" />
+        <h2>Weather in {country.capital[0]}</h2>
+        <p>temperature: {weather.main?.temp} Celsius</p>
+        {weather.weather && <img src={`http://openweathermap.org/img/wn/${weather.weather?.[0].icon}@2x.png`} alt={weather.weather?.[0].description} />}
+        <p>wind: {weather.wind?.speed} m/s</p> 
     </div>
 }
 
--- DataForCountries/src/services/Country.jsx
+++ DataForCountries/src/services/Country.jsx
@@ -1,7 +1,6 @@
 import axios from 'axios'
 const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api/'
 
-
 const getAll = () => {
     const request = axios.get(`${baseUrl}all`)
     return request.then(response => response.data)
@@ -12,4 +11,10 @@ const getByPartialName = (name) => {
   return request.then(response => response.data)
 }
 
-export default {getAll, getByPartialName}
+const getWeatherByLatLong = (lat, long) => {
+  console.log(import.meta.env.VITE_api_key)
+  const request = axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${import.meta.env.VITE_api_key}&units=metric`)
+  return request.then(response => response.data)
+}
+
+export default {getAll, getByPartialName, getWeatherByLatLong}
